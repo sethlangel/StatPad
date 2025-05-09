e708139652049de7f11546c4009a601b57fb82ed
Seth Langel <sethrlangel@gmail.com>
2025-05-09 16:20:26 -0700

    Changes "/" API response.

--- backend/backend.js
+++ backend/backend.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to StatPad!");
 });
 
 app.get('/users', async (req, res) => {

d4ce4525fa8316dd6e3e0f2e9e844a64e4b1683d
Swayam Chidrawar <schidraw@calpoly.edu>
2025-05-17 20:48:18 -0700

    Add: auth routes to backend

--- backend/backend.js
+++ backend/backend.js
@@ -4,6 +4,7 @@ import cors from "cors";
 // routes
 import home from "./routes/home.js";
 import users from "./routes/users.js";
+import auth from "./routes/auth.js";
 
 const app = express();
 const port = 8000;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.get("/", home);
 app.get("/users", users);
+app.use("/auth", auth);
 
 app.listen(port, () => {
   console.log(

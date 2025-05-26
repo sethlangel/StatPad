86db200f019add55ae8ea00e3b835bb7ba23818b
Connor S <csull54@gmail.com>
2025-05-26 15:03:53 -0700

    Basic backend auth + frontend communication

---
+++ backend/routes/watch.js
@@ -0,0 +1,148 @@
+import express from "express";
+import { supabase, supabaseAdmin } from "../supabase-client.js";
+import authenticateUser from "../middleware/auth.js";
+
+const router = express.Router();
+
+// ====================
+// POST /watch/stats-today?user_id=123
+// ====================
+router.get("/stats-today", async (req, res) => {
+  console.log("req = ", req.query);
+  const { userId } = req.query;
+
+  if (!userId) {
+    return res.status(400).json({ error: "Missing userId" });
+  }
+
+  const today = new Date().toISOString().split("T")[0];
+
+  const { data, error } = await supabase
+    .from("errors")
+    .select("*", { count: "exact", head: true }) // returns count only and not full rows
+    .eq("user_id", userId)
+    .gte("created_at", `${today}T00:00:00Z`);
+
+  console.log("data = ", data);
+  console.log("error = ", error);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json({ errors: data });
+});
+
+// ====================
+// POST /watch/new-game
+// ====================
+router.post("/new-game", authenticateUser, async (req, res) => {
+  console.log(req.user);
+
+  const { data, error } = await supabaseAdmin
+    .from("games")
+    .insert({ user_id: req.user.id })
+    .select();
+
+  console.log("data = ", data);
+  console.log("error = ", error);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(201).json({
+    message: "Game started",
+    gameId: data[0].id
+  });
+});
+
+// ====================
+// POST /watch/log-error
+// ====================
+const getErrorIdByName = async (errorName) => {
+  const { data, error } = await supabase
+    .from("error_list")
+    .select("id")
+    .eq("name", errorName)
+    .single();
+
+  if (error || !data) {
+    console.error("Error looking up error_id: ", error);
+    return null;
+  }
+
+  return data.id;
+};
+
+router.post("/log-error", async (req, res) => {
+  console.log("req = ", req);
+  const { gameId, errorType, userId } = req.body;
+
+  // Validate input
+  if (!gameId) {
+    return res.status(400).json({ error: "Missing gameId" });
+  }
+
+  if (!errorType) {
+    return res.status(400).json({ error: "Missing errorType" });
+  }
+
+  if (!userId) {
+    return res.status(400).json({ error: "Missing userId" });
+  }
+
+  // Validate if game exists
+  const { gameData, gameError } = await supabase
+    .from("games")
+    .select("id")
+    .eq("id", gameId)
+    .eq("user_id", userId)
+    .single();
+
+  if (gameError || !gameData) {
+    return res
+      .status(404)
+      .json({ error: "Game not found or unauthorized" });
+  }
+
+  // Validate error type
+  const { errorData, errorTypeError } = await supabase
+    .from("error_list")
+    .select("id")
+    .eq("name", errorType)
+    .single();
+
+  if (errorTypeError || !errorListData) {
+    return res
+      .status(400)
+      .json({ error: "Invalid error type" });
+  }
+
+  const errorId = await getErrorIdByName(errorType);
+  if (!errorId) {
+    return res
+      .status(400)
+      .json({ error: "Invalid error type" });
+  }
+
+  const { data, error } = await supabase.from("errors").insert({
+    game_id: gameId,
+    error_id: errorType,
+    user_id: userId
+  });
+
+  console.log("data = ", data);
+  console.log("error = ", error);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(201).json({
+    message: "Error logged",
+    data
+  });
+});
+
+export default router;
---
+++ frontend/src/app/watch/page.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { useAuth } from "../../hooks/useAuth";
+
+// Fetch today's error count
+const fetchTodaysErrorCount = async (userId: string) => {
+    const res = await fetch(`/watch/stats-today?userId=${userId}`);
+    const data = await res.json();
+
+    console.log("data in fetchTodaysErrorCount = ", data);
+    return (data.errors || []).length;
+};
+
+const Watch = () => {
+    const auth = useAuth();
+    //const [user, setUser] = useState<User | null>(null);
+
+    const [gameId, setGameId] = useState<number | null>(null);
+
+    // Track whether a game has started
+    const [gameStarted, setGameStarted] = useState(false);
+
+    // Store current game's error count
+    const [errorCount, setErrorCount] = useState(0);
+
+    // State to toggle the error type selector popup
+    const [showErrorSelector, setShowErrorSelector] = useState(false);
+
+    // On first render:
+    // 1. check for auth on mount
+    // 2. fetch today's error count
+    useEffect(() => {
+
+        if (!auth || !auth.isLoggedIn()) {
+            // TODO: handle not being signed in
+        } else {
+            // if (data && data.user) {
+            //     setUser(data.user);
+            //     fetchTodaysErrorCount(data.user.id).then(setErrorCount);
+            // }
+            // else {
+            //     window.location.href = "/login";
+            // }
+        }
+    }, []);
+
+    // Called when user clicks "Start Game"
+    const handleStartGame = async () => {
+        if (!auth || !auth.session) {
+            console.log("No auth or auth session.");
+            return;
+        }
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/watch/new-game`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${auth.session.access_token}`,
+            },
+            //body: JSON.stringify({ userId: user.id })
+        });
+
+        const data = await res.json();
+        console.log("data = ", data);
+        setGameId(data.gameId);
+        setGameStarted(true);
+    };
+
+    // Called when user clicks "Error" button
+    const handleLogError = () => setShowErrorSelector(true);
+
+    // Called when user selects error type
+    // const handleSelectErrorType = async (errorType: string) => {
+    //     setShowErrorSelector(false);
+
+    //     if (!user || !gameId) return;
+
+    //     await fetch("/watch/log-error", {
+    //         method: "POST",
+    //         headers: { "Content-Type": "application/json" },
+    //         body: JSON.stringify({
+    //             errorType,
+    //             gameId,
+    //             userId: user.id
+    //         })
+    //     });
+
+    //     setErrorCount((prev) => prev + 1);
+    // };
+
+    if (!auth || !auth.isLoggedIn())
+        return <div>Not logged in!</div>;
+
+    return (
+        <div>
+            {!gameStarted ? (
+                <>
+                    <h1>Today's Errors</h1>
+                    <p>{errorCount}</p>
+                    <button onClick={handleStartGame}>Start Game</button>
+                </>
+            ) : (
+                <>
+                    <h1>Unforced Errors</h1>
+                    <p>{errorCount}</p>
+                    <button onClick={handleLogError}>Error</button>
+
+                    {showErrorSelector && (
+                        <div>
+                            <h2>Select Error Type</h2>
+                            {/* <div>
+                                {["Dink", "Smash", "Volley"].map(
+                                    (errorType) => (
+                                        <button
+                                            key={errorType}
+                                            onClick={() =>
+                                                handleSelectErrorType(errorType)
+                                            }>
+                                            {errorType}
+                                        </button>
+                                    )
+                                )}
+                            </div> */}
+                        </div>
+                    )}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Watch;

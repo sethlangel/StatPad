a78d609660c2613ea32487209c7e14c067a5a66b
Seth Langel <sethrlangel@gmail.com>
2025-05-26 20:32:53 -0700

    Adds chart and table for /stats.

--- frontend/src/components/datepicker.tsx
+++ frontend/src/components/datepicker.tsx
@@ -5,15 +5,19 @@ export default function DatePicker(props: {
 }) {
 
     return (
-        <div className="flex flex-col gap-1">
-            {props.label && <h1 className="text-xs">{props.label}</h1>}
-            <input
-                type="date"
-                value={props.date}
-                onChange={(e) => props.onDateChange(e.target.value)}
-                className="input"
-            />
-        </div>
+        <div className="w-full flex flex-col items-center justify-center md:w-100">
+            {props.label && (
+                <h1 className="w-full text-left text-xs md:text-lg">{props.label}</h1>
+            )}
 
+            <div className="flex items-center flex-col gap-1 w-full">
+                <input
+                    type="date"
+                    value={props.date}
+                    onChange={(e) => props.onDateChange(e.target.value)}
+                    className="input w-full"
+                />
+            </div>
+        </div>
     )
 }

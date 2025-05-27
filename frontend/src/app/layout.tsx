c67f9a34553abdef0a25ff756d03ed6c610974ad
Seth Langel <sethrlangel@gmail.com>
2025-05-26 19:32:23 -0700

    Adds safearea for iOS.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,24 +1,17 @@
 'use client'
-
-import { AuthProvider } from "../hooks/useAuth"
 import '../styles/globals.css'
-import Navbar from "../components/navbar"
+import Navbar from '../components/navbar'
+import { AuthProvider } from '../hooks/useAuth'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="bg-dark-mode" lang="en">
-      <body className="text-white">
-        <main>
-            <AuthProvider>
-              <Navbar/>
-                {children}
-            </AuthProvider>
-        </main>
+    <html lang="en" data-theme="dark">
+      <body>
+          <AuthProvider>
+            <Navbar />
+            <main>{children}</main>
+          </AuthProvider>
       </body>
     </html>
-  )
+  );
 }

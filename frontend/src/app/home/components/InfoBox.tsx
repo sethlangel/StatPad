ce30db10017bc77154169d8980a593ee40a525cf
Connor S <csull54@gmail.com>
2025-06-03 21:33:49 -0700

    Seth is dumb

--- frontend/src/app/home/components/InfoBox.tsx
+++ frontend/src/app/home/components/InfoBox.tsx
@@ -20,9 +20,9 @@ export default function InfoBox(props: InfoData) {
             <h1 className="mb-2">{props.title}</h1>
 
             <div className="w-100 h-100 grid grid-cols-2 grid-rows-2 gap-3">
-                {props.data.map((info) => {
+                {props.data.map((info, index) => {
                     return (
-                        <div className="bg-base-300 rounded-2xl text-center" key={Math.random() * 1000}>
+                        <div className="bg-base-300 rounded-2xl text-center" key={index}>
                             <div className="flex items-center justify-center"><info.icon className="h-20 w-20 my-5" /></div>
                             <div className="font-bold text-2xl">{info.mainText}</div>
                             <div className="text-gray-300 inline-block align-bottom">{info.bottomText}</div>

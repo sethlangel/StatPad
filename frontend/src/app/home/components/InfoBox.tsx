055c3f836fa03673e7baa6a4c51df576d6e79cc6
Connor S <csull54@gmail.com>
2025-06-03 21:19:25 -0700

    Added unique keys

--- frontend/src/app/home/components/InfoBox.tsx
+++ frontend/src/app/home/components/InfoBox.tsx
@@ -13,7 +13,7 @@ export default function InfoBox(props: InfoData) {
             <div className="w-100 h-100 grid grid-cols-2 grid-rows-2 gap-3">
                 {props.data.map((info) => {
                     return (
-                        <div className="bg-base-300 rounded-2xl text-center">
+                        <div className="bg-base-300 rounded-2xl text-center" key={Math.random() * 1000}>
                             <div className="flex items-center justify-center"><info.icon className="h-20 w-20 my-5" /></div>
                             <div className="font-bold text-2xl">{info.mainText}</div>
                             <div className="text-gray-300 inline-block align-bottom">{info.bottomText}</div>
